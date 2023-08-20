804f76b974747cab23aaac92953ad4bc79ef74d5
Aziz <aziznal.dev@gmail.com>
2023-08-20 12:55:00 +0300

    add clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import type { AppProps } from "next/app";
+
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ClerkProvider {...pageProps}>
+      <main className={`h-full ${inter.className}`}>
+        <Component {...pageProps} />
+      </main>
+    </ClerkProvider>
+  );
 }
